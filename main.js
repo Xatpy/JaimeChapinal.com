e780b9383f1a767a3064d06d1d48afbde9c1ab25
Jaime Chapinal <jaimechapinal@Jaimes-MacBook-Air.local>
2021-05-29 22:19:03 +0200

    Adding service worker

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+const USE_SW = true;
+if (USE_SW && 'serviceWorker' in navigator) {
+    navigator.serviceWorker.register("/sw.js").then((reg) => {
+        console.log("Service worker registed", reg)
+    }).catch((err) => {
+        console.log("Errors registering service worker", err);
+    });
+}
+
+
 const initSaluteWave = () => {
     document.getElementById("emojiHi").onmouseenter = () => {
         const emojiHi = document.getElementById("emojiHi");
